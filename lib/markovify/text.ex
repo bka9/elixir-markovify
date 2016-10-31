04bbc5023282955cfe137fa347a19696a029387d
Kevin Anderson <kevin.anderson@octanner.com>
2016-10-31 07:13:28 -0600

    Fixed an error with floating point ranges.

--- lib/markovify/text.ex
+++ lib/markovify/text.ex
@@ -86,6 +86,6 @@ defmodule Markovify.Text do
   defp create_grams(grams, _, _, 0), do: grams
   defp create_grams(grams,words, overlap_over, gram_count) do
     index = gram_count - 1
-    create_grams(grams ++ [ Enum.slice(words, index..(index+overlap_over)) ], words, overlap_over, index)
+    create_grams(grams ++ [ Enum.slice(words, round(index)..round((index+overlap_over))) ], words, overlap_over, index)
   end
 end
